a057b4d61d5c92bc5e82d2205c4bd7ee40f91e2d
GBaykov <gleb.baykov99@gmail.com>
2024-05-13 03:00:32 +0300

    feat: add mantine theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
+import { AppMantineTheme } from './mantine.theme.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <MantineProvider theme={AppMantineTheme}>
+            <App />
+        </MantineProvider>
+    </React.StrictMode>,
+);
---
+++ src/mantine.theme.ts
@@ -0,0 +1,85 @@
+import { createTheme, rem } from '@mantine/core';
+
+export const AppMantineTheme = createTheme({
+    primaryColor: 'purple',
+    primaryShade: 5,
+    colors: {
+        purple: [
+            '#F5EDFF',
+            '#F2ECFA',
+            '#E5D5FA',
+            '#D1B4F8',
+            '#BD93F7',
+            '#9854F6',
+            '#541F9D',
+            '#4D1B95',
+            '#591FAA',
+            '#4D1896',
+        ],
+        grey: [
+            '#FFFFFF',
+            '#F5F5F6',
+            '#EAEBED',
+            '#D5D6DC',
+            '#C9CAD3',
+            '#ACADB9',
+            '#7B7C88',
+            '#60606D',
+            '#505363',
+            '#232134',
+        ],
+        yellow: [
+            '#fff9e1',
+            '#fff0cd',
+            '#fde09d',
+            '#fccf67',
+            '#fbc13b',
+            '#FAB005',
+            '#fab30e',
+            '#df9d00',
+            '#c78b00',
+            '#ac7700',
+        ],
+    },
+    fontFamily: 'Inter, sans-serif',
+    fontSizes: {
+        xs: rem(10),
+        sm: rem(11),
+        md: rem(14),
+        lg: rem(16),
+        xl: rem(20),
+    },
+    lineHeights: {
+        xs: '1',
+        sm: '1.25',
+        md: '1.4',
+        lg: '1.43',
+        xl: '1.6',
+    },
+
+    headings: {
+        // properties for all headings
+        fontWeight: '400',
+        fontFamily: 'Inter',
+        // properties for individual headings, all of them are optional
+        sizes: {
+            h1: { fontWeight: '700', fontSize: rem(32), lineHeight: '1.4' },
+            h2: { fontWeight: '600', fontSize: rem(24), lineHeight: '1.4' },
+            h3: { fontWeight: '600', fontSize: rem(20), lineHeight: '1.4' },
+            h4: { fontWeight: '700', fontSize: rem(20), lineHeight: '1' },
+            h5: { fontWeight: '700', fontSize: rem(16), lineHeight: '1.4' },
+            h6: { fontWeight: '400', fontSize: rem(16), lineHeight: '1.4' },
+        },
+    },
+    radius: { xs: rem(6), sm: rem(8), md: rem(10) },
+    defaultRadius: 'md',
+    spacing: { xs: rem(8), sm: rem(12), md: rem(16), lg: rem(18), xl: rem(24) },
+    cursorType: 'pointer',
+    breakpoints: {
+        xs: '30em',
+        sm: '48em',
+        md: '64em',
+        lg: '74em',
+        xl: '90em',
+    },
+});
